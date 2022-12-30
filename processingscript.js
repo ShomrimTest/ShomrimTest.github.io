8ac8d780bb678b9dfc6a3470959b018e519993b8
ShomrimTest <117776702+ShomrimTest@users.noreply.github.com>
2022-12-29 22:10:44 -0600

    Update processingscript.js

--- processingscript.js
+++ processingscript.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', function(event) {
 	var username = form.elements["username"]
 	
 	console.log(username)
-	console.log(username.value)
+	//console.log(username.value)
 	
 	//form.submit()
 });
